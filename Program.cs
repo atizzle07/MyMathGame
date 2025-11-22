1f866b7517fd8f5989797184cc0456cf37198e17
Allen Tripp <13400103+atizzle07@users.noreply.github.com>
2025-11-22 10:56:42 -0600

    1. Refined error handling logic and valid equation selection in division game 2. Wired up Game summary, not complete 3. Added section for game requirements

--- Program.cs
+++ Program.cs
@@ -22,12 +22,24 @@ Console.WriteLine();
 Console.WriteLine($"Thanks for playing {playerName}\n");
 Console.WriteLine("You will be pick questions for each math operation. For each correct answer you will gain points and for each wrong answer you will lose the same number of points (including multiplier).");
 
+
+// -------------- Game Requirements ------------------------------------------
+
+// TODO - You should record previous games in a List and there should be an option in the menu for the user to visualize a history of previous games.
+
+
+// --------------- Challenges ------------------------------------------------
+// UPGRADE - Add a timer to the program to see how quickly they can get to the point number
+// UPGRADE - Implement difficulty levels
+// UPGRADE - Implement a "random" game where the user will get a random operation rather than picking
+// UPGRADE - Just use a single method for the entire game instead of one for each operation.
+
 do
 {
-    Console.WriteLine("1. Addition\t\t(1.0x score multiplier)");
-    Console.WriteLine("2. Subtraction\t\t(1.1x score multiplier)");
-    Console.WriteLine("3. Multiplication\t(1.5x score multiplier)");
-    Console.WriteLine("4. Division\t\t(1.75x score multiplier)\n");
+    Console.WriteLine("1. Addition\t\t(1.0 points)");
+    Console.WriteLine("2. Subtraction\t\t(1.1 points)");
+    Console.WriteLine("3. Multiplication\t(1.5 points)");
+    Console.WriteLine("4. Division\t\t(1.75 points)\n");
 
     Console.WriteLine("Please select the math game you wish to play (Enter 1 - 4) or type 'exit' to finish playing:");
     input = Console.ReadLine();
@@ -39,54 +51,70 @@ do
     {
         case "1":
             Console.WriteLine("You selected 1. Addition");
-            additionGame();
-            Console.ReadLine();
+            AdditionGame();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(intercept: true);
             break;
         case "2":
             Console.WriteLine("You selected 2. Subtraction");
-            subtractionGame();
-            Console.ReadLine();
+            SubtractionGame();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(intercept: true);
             break;
         case "3":
             Console.WriteLine("You selected 3. Multiplication");
-            multiplyGame();
-            Console.ReadLine();
+            MultiplyGame();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(intercept: true);
             break;
         case "4":
             Console.WriteLine("You selected 4. Division");
-            divisionGame();
+            DivisionGame();
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey(intercept: true);
             break;
         case "exit":
-            Console.WriteLine("Thanks for playing! Press enter to leave...");
+            Console.WriteLine("Thanks for playing!");
+            GameSummary();
             Console.ReadLine();
-            //TODO: add in summary of score and how close player was to winning
             break;
         default:
             Console.WriteLine("Invalid Entry. Please try again.");
             break;
     }
 
-    void updateScore(decimal value, decimal multiplier)
+    void UpdateScore(decimal value, decimal multiplier)
     {
         // updates current score by value * multiplier
         playerScore += (value * multiplier);
     }
 
-    void ValidNumbers(int num1, int num2)
+    void ValidNumbers(ref int num1, ref int num2)
     {
         // ONLY REQUIRED FOR DIVISION GAME:
         // Takes in two values and determines if the result will be a whole integer. If the result is false this method will re-roll the numbers until a valid equationis reached
-        decimal resultCheck;
-
+        
+        int resultCheck = 1;
         do
         {
+            
+            if (num2 == 0) //check for divide by zero condition. If true, re-roll numbers
+            {
+                System.Console.WriteLine("Attempted to divide by zero, re-rolling numbers...");
+                num1 = rand.Next(11);
+                num2 = rand.Next(1,11);
+                continue;
+            }
+
             resultCheck = num1 % num2;
+            System.Console.WriteLine($"Num1: {num1}");
+            System.Console.WriteLine($"Num2: {num2}");
+            System.Console.WriteLine($"mod: {resultCheck}");
             if (resultCheck != 0)
             {
-                Console.WriteLine("Invalid equation, re-rolling numbers...\n");
+                Console.WriteLine($"Invalid equation, re-rolling numbers...\n");
                 num1 = rand.Next(11);
-                num2 = rand.Next(11);
+                num2 = rand.Next(1,11);
             }
             else
             {
@@ -96,7 +124,7 @@ do
         while (resultCheck != 0);
     }
 
-    void additionGame()
+    void AdditionGame()
     {
         string? input = null;
         expectedAnswer = num1 + num2;
@@ -109,20 +137,18 @@ do
 
         if (Convert.ToInt32(input) == expectedAnswer)
         {
-            updateScore(1, additionMultiplier);
+            UpdateScore(1, additionMultiplier);
             Console.WriteLine($"Correct! Your score is now {playerScore} points");
         }
 
         else
         {
-            updateScore(-1, additionMultiplier);
+            UpdateScore(-1, additionMultiplier);
             Console.WriteLine($"Sorry, that is incorrect. The answer is {expectedAnswer}. Your score is now {playerScore} points.");
         }
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey(intercept: true);
     }
 
-    void subtractionGame()
+    void SubtractionGame()
     {
         string? input = null;
         expectedAnswer = num1 - num2;
@@ -135,20 +161,18 @@ do
 
         if (Convert.ToInt32(input) == expectedAnswer)
         {
-            updateScore(1, subtractionMultiplier);
+            UpdateScore(1, subtractionMultiplier);
             Console.WriteLine($"Correct! Your score is now {playerScore} points");
         }
 
         else
         {
-            updateScore(-1, subtractionMultiplier);
+            UpdateScore(-1, subtractionMultiplier);
             Console.WriteLine($"Sorry, that is incorrect. The answer is {expectedAnswer}. Your score is now {playerScore} points.");
         }
-        Console.WriteLine("Press any key to contine...");
-        Console.ReadKey(intercept: true);
     }
 
-    void multiplyGame()
+    void MultiplyGame()
     {
         string? input = null;
         expectedAnswer = num1 * num2;
@@ -161,23 +185,21 @@ do
 
         if (Convert.ToInt32(input) == expectedAnswer)
         {
-            updateScore(1, multiplyMultiplier);
+            UpdateScore(1, multiplyMultiplier);
             Console.WriteLine($"Correct! Your score is now {playerScore} points");
         }
 
         else
         {
-            updateScore(-1, multiplyMultiplier);
+            UpdateScore(-1, multiplyMultiplier);
             Console.WriteLine($"Sorry, that is incorrect. The answer is {expectedAnswer}. Your score is now {playerScore} points.");
         }
-        Console.WriteLine("Press any key to contine...");
-        Console.ReadKey(intercept: true);
     }
 
-    void divisionGame()
+    void DivisionGame()
     {
         string? input = null;
-        ValidNumbers(num1, num2);
+        ValidNumbers(ref num1, ref num2);
         decimal expectedAnswer = num1 / num2;
         System.Console.WriteLine($"Num1: {num1}   Num2: {num2}   Answer: {expectedAnswer}");
         while (input == null)
@@ -188,17 +210,22 @@ do
 
         if (Convert.ToInt32(input) == expectedAnswer)
         {
-            updateScore(1, divisionMultiplier);
+            UpdateScore(1, divisionMultiplier);
             Console.WriteLine($"Correct! Your score is now {playerScore} points");
         }
 
         else
         {
-            updateScore(-1, divisionMultiplier);
+            UpdateScore(-1, divisionMultiplier);
             Console.WriteLine($"Sorry, that is incorrect. The answer is {expectedAnswer}. Your score is now {playerScore} points.");
         }
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey(intercept: true);
+    }
+
+    void GameSummary()
+    {
+        
+        Console.WriteLine();
+        // TODO - add summary output
     }
 }
 while (input != "exit");
